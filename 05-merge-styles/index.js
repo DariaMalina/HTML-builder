8c0193fba4d2a0e4245c8ddd8419712229680f0b
Daria Malina <gluto68@gmail.com>
2021-11-08 16:19:31 +0300

    feat: task rewritten to promises(task5)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,54 +2,44 @@ const path = require('path');
 const fs = require('fs');
 const folderToCopy = path.join(__dirname, 'styles');
 const folderToInsert = path.join(__dirname, 'project-dist');
-
-
-try {
-  fs.stat(
-    path.join(folderToInsert, 'bundle.css'), err => {
+const dateUpdate = 'File bundle.css updated successfully.';
+const dateCreate = 'File bundle.css has been successfully generated.';
+const wayUpBundle=path.join(folderToInsert, 'bundle.css');
+const func = async () => {
+  try {
+    fs.stat(path.join(folderToInsert, 'bundle.css'), err => {
       if (!err) {//если существует
-        fs.readdir(folderToCopy, (err, files) => {
-          if (err)
-            console.log(err);
-          else {
+        fs.promises.readdir(folderToCopy)
+          .then(files => {
             let arrFilesStyle = files;
             for (let i = 0; i < arrFilesStyle.length; i++) {
               if (path.extname(arrFilesStyle[i]) === '.css') {
                 let content = fs.readFileSync(path.join(folderToCopy, arrFilesStyle[i]), 'utf8');
-                fs.writeFile(path.join(folderToInsert, 'bundle.css'), content, err => {
-                  if (err)
-                    console.log(err);
-                });
+                fs.promises.writeFile(wayUpBundle, content + '\n');
               }
             }
-          }
-        });
-        console.log('file bundle.css updated successfully');
-      } else if (err.code === 'ENOENT') {
-        fs.readdir(folderToCopy, (err, files) => {
-          if (err)
-            console.log(err);
-          else {
+          });
+        console.log(dateUpdate);
+      } else if (err.code === 'ENOENT') {//если не существует
+        fs.promises.readdir(folderToCopy)
+          .then(files => {
             let arrFilesStyle = files;
             for (let i = 0; i < arrFilesStyle.length; i++) {
               if (path.extname(arrFilesStyle[i]) === '.css') {
                 let content = fs.readFileSync(path.join(folderToCopy, arrFilesStyle[i]), 'utf8');
-                fs.appendFile(path.join(folderToInsert, 'bundle.css'), content, err => {
-                  if (err)
-                    console.log(err);
-                });
+                fs.promises.appendFile(wayUpBundle, content + '\n');
               }
             }
-          }
-        });
-        console.log('file bundle.css has been successfully generated.');
-
+          });
+        console.log(dateCreate);
       }
     }
-  );
+    );
 
 
-} catch (error) {
+  } catch (error) {
+    console.log(error);
+  }
 
-  console.log(error);
-}
+};
+func();
