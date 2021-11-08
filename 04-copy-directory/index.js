076a9859b6adb99ccdcbfc720ecf8f89da31a958
Daria Malina <gluto68@gmail.com>
2021-11-08 14:53:25 +0300

    feat: task rewritten to promises(task4)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,28 +1,28 @@
+
 const fs = require('fs');
 const path = require('path');
-const copyFiles = () => {
-  fs.readdir(path.join(__dirname, 'files'), (err, files) => {//читаем содержимое нужной нам папки
-    if (err) throw err;
-    for (let i = 0; i < files.length; i++) {//терируемся по полученным данным
-      fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]), err => {//копирум по 1 файлу
-        if (err) throw err;
-      });
-    }
-  });
+const updatingFolderData='The data has been successfully updated.';
+const createFolderData='Folder created successfully.';
+
+const copyFiles = (message) => {
+  fs.promises.readdir(path.join(__dirname, 'files'))
+    .then(files => {
+      for (let i = 0; i < files.length; i++) {//терируемся по полученным данным
+        fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]), err => {//копирум по 1 файлу
+          if (err) throw err;
+        });
+      }
+      console.log(message);
+    });
 };
 let copyDir = async () => {
-  fs.stat(path.join(__dirname, 'files-copy'), function (err) {
-    if (!err) {//если папка уже имеется
-      copyFiles();
-      console.log('The data has been successfully updated.');
-    } else if (err.code === 'ENOENT') {//если папки не существует
-      fs.mkdir('files-copy', {recursive: true}, err => {//создаем папку
-        if (err) throw err;
-        copyFiles();
-      });
-      console.log('Folder created successfully');
-    }
-  });
+  fs.promises.stat(path.join(__dirname, 'files-copy'))
+    .then(copyFiles(updatingFolderData))
+    .catch(err => {
+      if (err.code === 'ENOENT') {//если папки не существует
+        fs.promises.mkdir('files-copy', {recursive: true})
+          .then(copyFiles(createFolderData));
+      }
+    });
 };
 copyDir();
-//The data has been successfully updated.
