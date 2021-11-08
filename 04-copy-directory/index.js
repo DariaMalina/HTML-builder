06376029ac82ca264b174229196204c8be8af81b
Daria Malina <gluto68@gmail.com>
2021-11-08 15:32:32 +0300

    feat: task rewritten to promises(task4/2)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 const path = require('path');
 const updatingFolderData='The data has been successfully updated.';
@@ -8,21 +7,21 @@ const copyFiles = (message) => {
   fs.promises.readdir(path.join(__dirname, 'files'))
     .then(files => {
       for (let i = 0; i < files.length; i++) {//терируемся по полученным данным
-        fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]), err => {//копирум по 1 файлу
-          if (err) throw err;
-        });
+        fs.promises.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]))
+          .catch(err => console.log(err));
       }
       console.log(message);
     });
 };
 let copyDir = async () => {
-  fs.promises.stat(path.join(__dirname, 'files-copy'))
-    .then(copyFiles(updatingFolderData))
-    .catch(err => {
-      if (err.code === 'ENOENT') {//если папки не существует
-        fs.promises.mkdir('files-copy', {recursive: true})
-          .then(copyFiles(createFolderData));
-      }
-    });
+  return fs.stat(path.join(__dirname, 'files-copy'), (err => {
+    if (!err) {
+      copyFiles(updatingFolderData);
+    } else if (err.code === 'ENOENT') {//если папки не существует
+      fs.promises.mkdir('files-copy', {recursive: true})
+        .then(copyFiles(createFolderData));
+    }
+  }));
+
 };
 copyDir();
